ce1751f92d65631d48fd51b7f9bab5add837c6f1
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-09-09 20:59:36 +0900

    Create 로또의 최고 순위와 최저 순위.py

---
+++ level 1/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,32 @@
+# 풀이
+def solution(lottos, win_nums):
+    answer = []
+    count = lottos.count(0) # 0 갯수 파악
+    same_num = 0
+    for i in lottos:
+      for j in win_nums:
+        if i == j:
+          same_num += 1
+        else:
+          pass
+      if same_num == 0:
+        answer=[7-same_num-count,6]
+        if count == 0:
+          answer=[6,6]
+      else:
+        answer = [7-same_num-count,7-same_num]
+    return answer
+
+# 훌륭한 코드
+# 랭크 리스트를 미리 만들어두는 방식!! 와우
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)    # lottos 안의 0의 개수를 반환
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+            
+    return rank[cnt_0 + ans],rank[ans]
