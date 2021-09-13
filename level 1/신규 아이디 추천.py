b3b29525eefcc29153a03877a7837300bd58661b
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-09-13 21:32:22 +0900

    Update 신규 아이디 추천.py
     풀이

--- level 1/신규 아이디 추천.py
+++ level 1/신규 아이디 추천.py
@@ -1,25 +1,28 @@
 def solution(new_id):
+    word = '~!@#$%^&*()=+[{]}:?,<>/'
     # 1단계
-    new_id = new_id.lower()
+    answer = new_id.lower()
     # 2단계
-    answer = ''
-    for word in new_id:
-        if word.isalnum() or word in '-_.':
-            answer += word
+    for i in range(len(word)):
+      answer = answer.replace(word[i],"")
     # 3단계
-    while '..' in answer:
-        answer = answer.replace('..', '.')
+    while ".." in answer:
+      answer = answer.replace("..",".")
     # 4단계
-    answer = answer[1:] if answer[0] == '.' and len(answer) > 1 else answer
-    answer = answer[:-1] if answer[-1] == '.' else answer
+    if answer[0]==".":
+      answer = answer[1:]
+    if len(answer) > 1:
+      if answer[len(answer)-1] == ".":
+        answer = answer[:len(answer)-1]   
     # 5단계
-    answer = 'a' if answer == '' else answer
+    if len(answer) == 0:
+      answer = "a"
     # 6단계
     if len(answer) >= 16:
-        answer = answer[:15]
-        if answer[-1] == '.':
-            answer = answer[:-1]
+      answer = answer[:15]
+      if answer[len(answer)-1] == ".":
+        answer = answer[:len(answer)-1]   
     # 7단계
-    if len(answer) <= 3:
-        answer = answer + answer[-1] * (3-len(answer))
+    if len(answer) < 3:
+      answer = answer + (3-len(answer))*answer[len(answer)-1]
     return answer
