e7a1f705bc740be83f8089cf9cf0c6ee64984460
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-09-14 01:47:38 +0900

    Update 키패드 누르기.py

--- level 1/키패드 누르기.py
+++ level 1/키패드 누르기.py
@@ -1,4 +1,39 @@
+# wrong solution
+def solution(numbers, hand):
+    answer = ''
+    l_hand = 10
+    r_hand = 12
+    for j in range(len(numbers)):
+      if numbers[j] == 0:
+        numbers[j]=11
+    for i in numbers:
+      if i in [1,4,7]:
+        answer = answer + "L"
+        l_hand = i # 1 4
+      elif i in [3,6,9]:
+        answer = answer + "R"
+        r_hand = i # 3
+      else:
+        print()
+        if abs(i - (l_hand + 2)) < abs(i - r_hand): # 0
+          if abs(i-r_hand) == 3:
+            r_hand = id
+            answer = answer + "R"
+          else:
+            l_hand = i
+            answer = answer + "L"
+        elif abs(i - l_hand + 2) > abs(i - r_hand): # 7
+          if abs(i-l_hand) == 3:
+            l_hand = i
+            answer = answer + "L"
+          else:
+            r_hand = i
+            answer = answer + "R"
+        else:
+          answer = answer + hand[0].upper()
+    return answer
 
+# right solution
 def solution(numbers, hand):
     answer = ''
     lastL = 10
