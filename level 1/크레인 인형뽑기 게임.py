4ce0844f897e4afce3ea337dccb4f404f701bb94
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-09-14 09:43:16 +0900

    Update 크레인 인형뽑기 게임.py
    풀이 성공~!

--- level 1/크레인 인형뽑기 게임.py
+++ level 1/크레인 인형뽑기 게임.py
@@ -1,18 +1,14 @@
 def solution(board, moves):
   result = []
+  answer = 0
   for i in moves:
     for j in range(len(board)):
       if board[j][i-1] is not 0:
         result.append(board[j][i-1])
+        if len(result)>1 and result[len(result)-2] == result[len(result)-1]:
+          del(result[len(result)-2])
+          del(result[len(result)-1])
+          answer += 2
         board[j][i-1] = 0
-        print(result)
         break
-  # while True:
-  for j in range(len(result)):
-    print(result[j],result[j+1])
-    # if result[j] == result[j+1]:
-    #   result = result.remove(result[j])
-  print(result)
   return answer
-
-solution([[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]],	[1,5,3,5,1,2,1,4])
