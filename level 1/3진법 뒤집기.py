fdd1fc69df97da556ca5ba5b298beb1926a307da
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-09-25 23:38:14 +0900

    Update 3진법 뒤집기.py

--- level 1/3진법 뒤집기.py
+++ level 1/3진법 뒤집기.py
@@ -1,1 +1,20 @@
+# my solution
+def solution(n):
+    reverse = []
+    answer = 0
+    while n > 0:
+        reverse.append(n%3)
+        n=n//3
+    for i in range(len(reverse)):
+        answer += 3**(len(reverse)-1-i)*reverse[i]
+    return answer
 
+# right solution
+def solution(n):
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
+        n = n // 3
+
+    answer = int(tmp, 3)
+    return answer
