d5e929494ebb9c3d062646e1f44cd73856709957
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-09-28 13:20:30 +0900

    Update 2016년.py

--- level 1/2016년.py
+++ level 1/2016년.py
@@ -1,3 +1,5 @@
-def solution(a, b):
-    answer = ''
-    return answer
+import datetime
+
+def solution(a,b):
+    t = 'MON TUE WED THU FRI SAT SUN'.split()
+    return t[datetime.datetime(2016, a, b).weekday()]
