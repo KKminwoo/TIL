8b5d1a09101aba6e03284f2a5f98d8c93f0ecca0
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-09-28 13:18:49 +0900

    Update 두 개 뽑아서 더하기.py

--- level 1/두 개 뽑아서 더하기.py
+++ level 1/두 개 뽑아서 더하기.py
@@ -1,6 +1,13 @@
+# my solution
 import itertools
 def solution(numbers):
-    answer = []
+    answer = set()
     nCr = itertools.combinations(numbers,2)
-    print(nCr)
-    return answer
+    for i in list(nCr):
+        answer.add(sum(i))
+    return sorted(answer)
+
+# right solution (wow~)
+from itertools import combinations
+def solution(numbers):
+    return sorted(list(set([sum([i,j]) for i,j in combinations(numbers,2)])))
