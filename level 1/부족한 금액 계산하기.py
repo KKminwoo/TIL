71e328064102fcd12944998f5be0d5dc59b1c24e
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-10-19 13:52:04 +0900

    Update 부족한 금액 계산하기.py

--- level 1/부족한 금액 계산하기.py
+++ level 1/부족한 금액 계산하기.py
@@ -1,4 +1,13 @@
+# perfect solution
 def solution(price, money, count):
-    answer = -1
-
-    return answer
+    return max(0,price*(count+1)*count//2-money)
+ 
+# my solution
+def solution(price, money, count):
+    answer = 0
+    for i in range(1,count+1):
+        answer += price*i
+    if answer-money <= 0:
+        return 0
+    else:
+        return answer-money
