293108136c103af76d5aa891003de8668e0e5c58
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-10-29 15:04:40 +0900

    Update 문자열 압축.py

--- level2/문자열 압축.py
+++ level2/문자열 압축.py
@@ -1,1 +1,22 @@
-
+# not solve
+def solution(s):
+    num = s[0]
+    count = 0
+    word = ''
+    answer = []
+    for i in s:
+        if i==num:
+            count += 1
+            num = i
+            # print(i,"coount 1")
+        else:
+            if count <= 1:
+                word += num
+                # print(num'one arg')
+            else:
+                word = word + str(count) + num
+            num = i
+            count = 1
+        print(word)
+            
+    return answer
