0ce37c9be1a938af83301ac58fc0de220eb87e71
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-11-04 10:57:43 +0900

    Update 124 나라의 숫자.py

--- level2/124 나라의 숫자.py
+++ level2/124 나라의 숫자.py
@@ -1,16 +1,15 @@
 def solution(n):
     answer = ''
-    if n <= 3:
-        answer = '124'[n-1]
-    else:
+    while n > 0:
         n -= 1
-        answer =  + '124'[n%3]
-        
-    
+        answer = '124'[n%3] + answer
+        n //= 3
     return answer
 
-
-# 11 42
-# 12 44
-# 13 111
-# 14 112
+# right solution -> 알고리즘을 잘 생각해내보자;; 항상 한 발짜국 더 못가는 것 같음
+def change124(n):
+    if n<=3:
+        return '124'[n-1]
+    else:
+        q, r = divmod(n-1, 3) 
+        return change124(q) + '124'[r]
