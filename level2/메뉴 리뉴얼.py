f9f1088e77c00505afbec867cfb4e448fbf9854f
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-11-24 17:01:00 +0900

    Update 메뉴 리뉴얼.py

--- level2/메뉴 리뉴얼.py
+++ level2/메뉴 리뉴얼.py
@@ -2,13 +2,15 @@ from itertools import combinations
 
 def solution(orders, course):
     answer = []
-    count = []
+    max_num = 0
     for j in course:
+        count_list = []
         print(j,"개 선택")
         for i in orders:
             for k in list(combinations(i,j)):
-                if 
-                count.append(k)
-        print(count)
-            # print("\n")
+                count_list.append(''.join(k))
+                if ''.join(k) in count_list and count_list.count(''.join(k)) >= max_num:
+                    print(''.join(k),count_list.count(''.join(k)))
+                    max_num = count_list.count(''.join(k))
+                print(count_list)
     return answer
