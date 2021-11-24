6b27ebf8e3048e418497ff453b261599834c7eb9
minwoo Kim <47807421+KKminwoo@users.noreply.github.com>
2021-11-24 22:15:50 +0900

    Update 메뉴 리뉴얼.py

--- level2/메뉴 리뉴얼.py
+++ level2/메뉴 리뉴얼.py
@@ -1,16 +1,20 @@
+# Counter 함수 익히기
+# combination 함수 익히기
+
 from itertools import combinations
+from collections import Counter
 
 def solution(orders, course):
     answer = []
-    max_num = 0
+    counter_num = {}
     for j in course:
         count_list = []
-        print(j,"개 선택")
         for i in orders:
             for k in list(combinations(i,j)):
-                count_list.append(''.join(k))
-                if ''.join(k) in count_list and count_list.count(''.join(k)) >= max_num:
-                    print(''.join(k),count_list.count(''.join(k)))
-                    max_num = count_list.count(''.join(k))
-                print(count_list)
-    return answer
+                count_list.append(''.join(sorted(k)))
+        counter_num = Counter(count_list).most_common()
+        for s in counter_num:
+            if counter_num[0][1] > 1 and s[1] == counter_num[0][1]:
+                print(counter_num[0][1], s[0])
+                answer.append(s[0])
+    return sorted(answer)
