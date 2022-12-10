615bb5bcd623192b3e3b856c836aae09da5eb25e
KKminwoo <ehdud6960@naver.com>
2022-12-10 17:21:33 +0900

    Update : Study 07 chapter

--- cpp_study/test.cpp
+++ cpp_study/test.cpp
@@ -1,22 +1,14 @@
 #include <iostream>
-#include <cctype>
+const double *f1(const double ar[], int n);
+const double *f2(const double [], int);
+const double *f3(const double *, int);
 int main() {
     using namespace std;
-    cout << "input text, end with @\n";
-    char ch;
-    int whitespace = 0;
-    int digits = 0;
-    int chars = 0;
-    int punct = 0;
-    int others = 0;
-
-    cin.get(ch);
-    while (ch != '@')
-    {
-        if (isalpha(ch))
-            chars++;
-        else if (isspace(ch))
-            whites
-    }
-    
+    double av[3] = {112.3,132.1,141.1};
+    const double *(*p1)(const double *, int) = f1;
+    auto p2 = f2;
+    cout << (*p1)(av,3) << ": " << *(*p1)(av,3) << endl;
+    cout << p2(av,3) << ": " << *p2(av,3) << endl;
+   
+    return 0;
  }
